2ac7d8ab8da31807947b6b1914c82f25839960d4
Saku <myllymakisaku@gmail.com>
2019-02-13 12:02:35 +0200

    class Wall added

--- src/Demo.scala
+++ src/Demo.scala
@@ -17,7 +17,7 @@ import scalafx.scene.Cursor
 
 object Demo extends JFXApp {
   
-  private var walls = Vector[Line]()
+  private var walls = Vector[Wall]()
   
   val screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize()
   val screenHeight = screenSize.getHeight().toInt
@@ -26,7 +26,6 @@ object Demo extends JFXApp {
   val windowWidth = 1280
   val windowHeight = 720
   
-//  val frameRate = 60 
   val fov = 100 //Field of view in degrees
   val fovInRadians = math.toRadians(fov) //Converted to radians
   val sensitivity = 5 //Determines how much moving the mouse turns the camera. 1 is very low, 10 is very high
@@ -62,17 +61,16 @@ object Demo extends JFXApp {
     if(arr(rowIndex)(charIndex) == '#') {
         val vecs = Vector(new Vec(charIndex, -rowIndex), new Vec(charIndex + 1, -rowIndex), 
                           new Vec(charIndex + 1, -rowIndex - 1), new Vec(charIndex, -rowIndex - 1))
-        walls = walls ++: Vector(new Line(vecs(0), vecs(1)), new Line(vecs(1), vecs(2)), new Line(vecs(2), vecs(3)), new Line(vecs(3), vecs(0)))
+        walls = walls ++: Vector(new Wall(vecs(0), vecs(1)), new Wall(vecs(1), vecs(2)), new Wall(vecs(2), vecs(3)), new Wall(vecs(3), vecs(0)))
       }
   }}
   
-  var wallsToRemove = Vector[Line]()
+  var wallsToRemove = Vector[Wall]()
   
   //Remove walls that are covered by other walls and can thus never be seen to reduce performance issues,
   //if there are 
   for(wall <- walls) {
-    if(walls.exists{another => another.v1.x == wall.v2.x && another.v1.y == wall.v2.y && 
-                               another.v2.x == wall.v1.x && another.v2.y == wall.v1.y }) {
+    if(walls.filter(another => another != wall).exists{another => another.equals(wall)}) {
       wallsToRemove = wallsToRemove :+ wall
     }
   }
@@ -98,13 +96,14 @@ object Demo extends JFXApp {
   def moveRight(elapsedTime: Double)   = move(math.cos(this.heading), -math.sin(this.heading), elapsedTime)
   def moveLeft(elapsedTime: Double)    = move(-math.cos(this.heading), math.sin(this.heading), elapsedTime)
   def moveBack(elapsedTime: Double)    = move(-math.sin(this.heading), -math.cos(this.heading), elapsedTime)
+  def moveFR(elapsedTime: Double)      = move(math.sin(this.heading + math.Pi / 4), math.cos(this.heading + math.Pi / 4), elapsedTime)
+  def moveFL(elapsedTime: Double)      = move(math.sin(this.heading - math.Pi / 4), math.cos(this.heading - math.Pi / 4), elapsedTime)
+  def moveBR(elapsedTime: Double)      = move(-math.sin(this.heading - math.Pi / 4), -math.cos(this.heading - math.Pi / 4), elapsedTime)
+  def moveBL(elapsedTime: Double)      = move(-math.sin(this.heading + math.Pi / 4), -math.cos(this.heading + math.Pi / 4), elapsedTime)
   
   def move(xChange: Double, yChange: Double, elapsedTime: Double) = {
-    //In case the player is moving in two directions at once, it won't look like the player is moving faster
-    val coEfficient = {
-      if(movingStraight && movingSideWays) 3 * elapsedTime / math.sqrt(2)
-      else 3 * elapsedTime
-    }
+    //The multiplier 3 is here to make movement faster 
+    val coEfficient = 3 * elapsedTime
     val wallsNearby = walls.filter(wall => new Vec(wall.v1.x - location.x, wall.v1.y - location.y).length < 2)
     val newXLocation = new Vec(this.location.x + coEfficient * xChange, this.location.y)
     if(wallsNearby.forall (wall => new Line(this.location, newXLocation).lineIntersect(wall).isEmpty )) this.location = newXLocation
@@ -112,23 +111,22 @@ object Demo extends JFXApp {
     if(wallsNearby.forall (wall => new Line(this.location, newYLocation).lineIntersect(wall).isEmpty )) this.location = newYLocation
   }
   
-  def pointWithinFov(point: Vec) = {
-    val headingUnitized = new Vec(math.sin(heading), math.cos(heading))
-    val diff = new Vec(point.x - location.x, point.y - location.y).unitize()
-    //Take the dot product of the heading and diff
-    val dotProduct = headingUnitized.x * diff.x + headingUnitized.y * diff.y
-    dotProduct > math.cos(fovInRadians / 2)
-  }
   def wallWithinFov(wall: Line) = {
+    def pointWithinFov(point: Vec) = {
+      val headingUnitized = new Vec(math.sin(heading), math.cos(heading))
+      val diff = new Vec(point.x - location.x, point.y - location.y).unitize()
+      //Take the dot product of the heading and diff
+      val dotProduct = headingUnitized.x * diff.x + headingUnitized.y * diff.y
+      dotProduct > math.cos(fovInRadians / 2)
+    }
     pointWithinFov(wall.v1) || pointWithinFov(wall.v2) || 
     wall.lineIntersect(new Line(location, new Vec(location.x + 100 * math.sin(heading), location.y + 100 * math.cos(heading)))).isDefined
   }
   
   def paint() = {
     var rectangles = Vector[Rectangle]() //All pieces of wall that will be drawn on the screen
-    //For excluding the walls which are outside the current fov
+    //Exclude the walls that are outside the current fov
     val wallsInsideFov = walls.filter(wall => wallWithinFov(wall))
-    println(wallsInsideFov.size)
     for(x <- 0 until windowWidth) { //Go through each ray
       //The angle of each ray depends on the field of view and the width of the window
       val rayHeading = heading + (x - windowWidth / 2) * fovInRadians / windowWidth 
@@ -140,17 +138,17 @@ object Demo extends JFXApp {
       for(wall <- wallsInsideFov) { 
         val intersection = ray.lineIntersect(wall)
         intersection match {
-          case Some(intersection) => {
+          case Some(intersection) => 
             val distance = new Vec(intersection.x - ray.v1.x, intersection.y - ray.v1.y).length
+            //The rectangle height is divided by math.cos(heading - rayHeading) to get rid of the fisheye effect
             val rectHeight = (1.5 * windowHeight / distance / fovInRadians / math.cos(heading - rayHeading)).toInt
-            //Adjust 
-            color = new Color((255 / (0.3 * distance + 1)).toInt, 0, 0)//.deriveColor(1, 1, 1 / (0.3 * distance + 0.1), 1)
+            //Adjust the brightness of the color according to distance
+            color = new Color((255 / (0.3 * distance + 1)).toInt, 0, 0)
             intersections = intersections :+ new Rectangle(x, distance, rectHeight, color)
-          }
-          case None => 
+          case None =>
         }
       }
-      
+      //Choose the closest of the intersections 
       if(intersections.nonEmpty) {
         rectangles = rectangles :+ intersections.minBy(_.distance)
       }
@@ -173,10 +171,18 @@ object Demo extends JFXApp {
     val elapsedTime = (t - previousTime) / 1000000000.0 //The elapsed time in seconds
     previousTime = t
     fps = (1 / elapsedTime).toInt
-    if(wPressed && !sPressed) moveForward(elapsedTime)
-    if(aPressed && !dPressed) moveLeft(elapsedTime)
-    if(sPressed && !wPressed) moveBack(elapsedTime)
-    if(dPressed && !aPressed) moveRight(elapsedTime)
+    if(wPressed && !sPressed) {
+      if(aPressed && !dPressed) moveFL(elapsedTime)
+      else if(dPressed && !aPressed) moveFR(elapsedTime)
+      else moveForward(elapsedTime)
+    }
+    else if(sPressed && !wPressed) {
+      if(aPressed && !dPressed) moveBL(elapsedTime)
+      else if(dPressed && !aPressed) moveBR(elapsedTime)
+      else moveBack(elapsedTime)
+    }
+    else if(aPressed && !dPressed) moveLeft(elapsedTime)
+    else if(dPressed && !aPressed) moveRight(elapsedTime)
     paint() 
     gc.setFill(new Color("white"))
     gc.fillText(fps.toString, 10, 10, 100)
@@ -205,8 +211,6 @@ object Demo extends JFXApp {
   private var aPressed = false
   private var sPressed = false
   private var dPressed = false
-  def movingStraight = (wPressed && !sPressed) || (sPressed && !wPressed) 
-  def movingSideWays = (aPressed && !dPressed) || (dPressed && !aPressed) 
   
   canvas.onKeyPressed = (e: KeyEvent) => {
     e.code match {
--- src/Vec.scala
+++ src/Vec.scala
@@ -4,6 +4,8 @@ import scalafx.scene.paint.Color
 
 class Vec(val x: Double, val y: Double) {
   
+  def equals(another: Vec) = this.x == another.x && this.y == another.y
+  
   val length = math.sqrt(this.x * this.x + this.y * this.y) 
   
   //Determines the angle between two vectors, TODO
@@ -11,48 +13,8 @@ class Vec(val x: Double, val y: Double) {
   
   override def toString = "Vector at (" + this.x + ", " + this.y + ")" 
 }
-//This class could be used to create walls with different colours
-//class Wall(v1: Vec, v2: Vec) extends Line(v1, v2) {
-//  val color = {
-//    val r = scala.util.Random.nextInt(3)
-//    r match {
-//      case 0 => "red"
-//      case 1 => "green"
-//      case 2 => "blue"
-//    }
-//  }
-//}
-class Line(val v1: Vec, val v2: Vec) {
-  //Checks whether two lines share the same vertices, walls are always created from the text file characters
-  //in a clockwise manner. Hence the first vertex of one wall should be the second vertex of another. 
-  def equals(another: Line) = {
-    this.v1.x == another.v2.x && this.v1.y == another.v2.y && this.v2.x == another.v1.x && this.v2.y == another.v1.y
-  }
-  /* The calculation of the intersection:
-   * If two lines intersect, then there exist t and s 
-   * so that this.v1 + s * u = wall.v1 + t * v, where
-   * s and t are scalars, u = this.v2 - this.v1 and
-   * v = wall.v2 - wall.v1. s and t can be solved from 
-   * this equation by using cross products to eliminate
-   * s (when solving for t) and t (when solving for s)
-   * s * u x u = 0 and t * v x v = 0
-   */
-  def lineIntersect(wall: Line) = {
-    val u = new Vec(this.v2.x - this.v1.x, this.v2.y - this.v1.y)
-    val v = new Vec(wall.v2.x - wall.v1.x, wall.v2.y - wall.v1.y)
-    
-    val vxu = v.x * u.y - v.y * u.x //Magnitude of the cross product of v and u
-    val w = new Vec(this.v1.x - wall.v1.x, this.v1.y - wall.v1.y)
-    val t = (w.x * u.y - w.y * u.x) / vxu
-    val s = (w.x * v.y - w.y * v.x) / vxu      
-    if(t >= 0 && t <= 1 && s >= 0 && s <= 1) {
-      Some(new Vec(wall.v1.x + t * v.x, wall.v1.y + t * v.y))
-    }
-    else {
-      None
-    }
-  }
-}
+
+
 //This class is used to draw rectangles on the screen according to where rays intersect with walls
 class Rectangle(val screenPosition: Double, val distance: Double, val height: Int, val color: Color)
 
