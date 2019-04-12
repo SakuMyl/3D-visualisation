6241e28f7222de64de4c1c12204f89bd7ae74eee
myllyms3 <saku.myllymaki@aalto.fi>
2019-04-12 16:51:04 +0300

    Changes in imports

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,22 +4,16 @@ import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.canvas.Canvas
 import scalafx.scene.Scene
-import scalafx.scene.input.KeyEvent
+import scalafx.scene.input.{KeyCode,KeyEvent}
+import javafx.scene.input.KeyCombination
 import scalafx.animation.AnimationTimer
-import scalafx.scene.input.MouseEvent
-import scalafx.scene.input.KeyCode
 import scalafx.scene.Cursor
 import scalafx.scene.paint.Color
-import scalafx.scene.input.KeyCodeCombination
-import javafx.scene.input.KeyCombination
-import scalafx.scene.text.Font
+import scalafx.scene.text.{Font, TextAlignment}
 import scalafx.scene.control.Alert
-import scalafx.scene.control.Alert.AlertType
-import scalafx.scene.control.ButtonType
-import scalafx.scene.image.Image
-import scalafx.scene.image.WritableImage
+import Alert.AlertType
+import scalafx.scene.image.{Image, WritableImage}
 import javafx.stage.Screen
-import scalafx.scene.text.TextAlignment
 
 object Demo extends JFXApp {
   
